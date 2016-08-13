d9dfda9e7dd918775c9adeef7c9b2f703b5072a0
Xavi <xavifuefer@gmail.com>
2016-08-13 18:37:55 +0100

    hello world

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const webpack = require('webpack');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: [
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
+    './src/index.js',
+  ],
+  module: {
+    loaders: [{
+      test: /\.js?$/,
+      exclude: /node_modules/,
+      loader: 'babel',
+    }],
+  },
+  resolve: {
+    extensions: ['', '.js'],
+  },
+  output: {
+    path: 'dist',
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  devServer: {
+    contentBase: './dist',
+    hot: true,
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin(),
+  ],
+};
