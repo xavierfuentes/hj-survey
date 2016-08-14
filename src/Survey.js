028c948bdf9563307c7075b1f39a111412c31a66
Xavi <xavifuefer@gmail.com>
2016-08-14 17:10:30 +0100

    refactor the rendering process

--- src/Survey.js
+++ src/Survey.js
@@ -1,57 +1,103 @@
-import stepsTemplate from './steps.html';
-import surveyTemplate from './survey.html';
+import uuid from 'uuid';
+
+const _factory = document.createElement('div');
+const _templates = {
+  container: '<div></div>',
+  dialog: '<div id="survey-dialog"></div>',
+  content: '<div id="survey-content"></div>',
+  header: '<div id="survey-header"></div>',
+  step: '<div id="survey-step"></div>',
+  footer: '<div id="survey-footer"></div>',
+};
+const _initialState = {
+  initialStep: 1,
+  activeStep: 1,
+  maxStep: 4,
+};
+
+/**
+ * Builds an HTML node from a string
+ * @param  {String} content The HTML to render
+ * @return {Object}         DOM node
+ */
+function buildNode(content) {
+  _factory.innerHTML = '';
+  _factory.innerHTML = content;
+
+  return _factory.childNodes[0];
+}
 
 /**
  * @class Survey
  */
 class Survey {
-
   constructor() {
-    // Create options by extending defaults with the passed in settings
-    this.state = {
-      activeStep: 1,
-      maxStep: 4,
-    };
+    this.id = uuid.v4();
+    this.state = Object.assign({}, _initialState);
 
-    this.show = this._renderSurvey.bind(this);
+    this._html = {};
+    this._templates = Object.assign({}, _templates);
 
-    this._stepsHTML = this._buildFragment(stepsTemplate);
-    this._surveyHTML = this._buildFragment(surveyTemplate);
+    this._render();
   }
 
-  _renderSurvey() {
-    // 2. get the current step (from LS?)
-    // 3. remove the previous step
-    // 4. build the current step
-    const step = this._getStep(this.state.activeStep);
-    // 5. append the current step to the survey
-    this._surveyHTML.getElementById('steps-wrapper').appendChild(step);
-    // 6. render the survey
-    document.body.appendChild(this._surveyHTML);
+  show() {
+    const html = this._html;
+    console.log(html);
+    document.body.appendChild(html.container);
   }
 
-  /**
-   * Selects the step based on its ID
-   * @param  {Number} [step=1] The number of the step
-   * @return {Object}          The selected node
-   */
-  _getStep(step = 1) {
-    return this._stepsHTML.getElementById(`step${step}`);
+  _render() {
+    const html = this._html;
+    const tpls = this._templates;
+
+    html.container = buildNode(tpls.container);
+    html.dialog = buildNode(tpls.dialog);
+    html.content = buildNode(tpls.content);
+    html.header = buildNode(tpls.header);
+    html.step = buildNode(tpls.step);
+    html.footer = buildNode(tpls.footer);
+
+    // sets a unique id for the container
+    html.container.id = this.id;
+
+    this._renderHeader();
+    this._renderSteps();
+    this._renderFooter();
+
+    html.dialog.appendChild(html.content);
+    html.container.appendChild(html.dialog);
+
+    return this;
   }
 
-  /**
-   * Builds a document fragment from an HTML string
-   * @param  {String} content The HTML to render
-   * @return {Object}         Document fragment
-   */
-  _buildFragment(content) {
-    const contentHolder = document.createElement('div');
-    const docFrag = document.createDocumentFragment();
+  _renderHeader() {
+    const html = this._html;
 
-    contentHolder.innerHTML = content;
-    docFrag.appendChild(contentHolder);
+    html.header.innerHTML = '<div id="survey-title">One fine survey</div>';
+    html.content.appendChild(html.header);
+  }
 
-    return docFrag;
+  _renderSteps() {
+    const html = this._html;
+
+    html.step.innerHTML = `
+      <div id="survey-step-1">step #1</div>
+      <div id="survey-step-2">step #2</div>
+      <div id="survey-step-3">step #3</div>
+      <div id="survey-step-4">step #4</div>
+    `;
+    html.content.appendChild(html.step);
+  }
+
+  _renderFooter() {
+    const html = this._html;
+
+    html.footer.innerHTML = `
+      <button>Previous</button>
+      <button>Next</button>
+    `;
+    html.content.appendChild(html.footer);
   }
 }
 
