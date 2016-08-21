99720df4512656b33999a45641a41c5a1eb1c5ec
Xavi <xavifuefer@gmail.com>
2016-08-21 02:52:40 +0100

    add validation to every step

--- src/Survey.js
+++ src/Survey.js
@@ -3,14 +3,50 @@ import { createStore } from 'redux';
 // initial state
 const initialState = {
   step: 1,
-  totalSteps: 5,
   shouldRender: true,
-  isValid: false,
   steps: {
-    step1: {
-      name: { value: '', isValid: false },
-      email: { value: '', isValid: false },
-      type: { value: '', isValid: false },
+    1: {
+      id: 1,
+      title: 'Personal Details',
+      isValid: false,
+      fields: {
+        name: { required: true, value: '' },
+        email: { required: true, value: '' },
+        type: { required: true, value: '' },
+      },
+    },
+    2: {
+      id: 2,
+      title: 'Business Details',
+      isValid: false,
+      fields: {
+        'business.name': { required: true, value: '' },
+        'business.size': { required: true, value: '' },
+      },
+    },
+    3: {
+      id: 3,
+      title: 'Location Details',
+      isValid: false,
+      fields: {
+        address: { required: true, value: '' },
+        euresident: { required: true, value: null },
+      },
+    },
+    4: {
+      id: 4,
+      title: 'Hobbies',
+      isValid: false,
+      fields: {
+        book: { required: false, value: '' },
+        'colors.red': { required: false, value: null },
+        'colors.green': { required: false, value: null },
+        'colors.blue': { required: false, value: null },
+      },
+    },
+    5: {
+      id: 5,
+      title: 'You made it!',
     },
   },
 };
@@ -22,35 +58,47 @@ const validateForm = (payload) => ({ type: 'VALIDATE_FORM', payload });
 
 // reducer
 function survey(state = initialState, action) {
-  console.log('survey', action);
+  console.log('action dispatched ->', action);
 
   switch (action.type) {
     case 'NEXT_STEP': {
-      // add a special condition in which we skip a step
-      const increment = state.step === 1 && state.steps.step1.type.value === '' ? 1 : 2;
+      // Special condition for skipping step 2 and going straight to step 3
+      const increment = state.step === 1 && state.steps[1].fields.type.value === 'individual' ? 2 : 1;
 
       return Object.assign({}, state, {
-        step: state.step + increment,
+        step: state.steps[state.step].isValid ? state.step + increment : state.step,
         shouldRender: true,
-        isValid: false,
       });
     }
 
-    case 'PREVIOUS_STEP': return Object.assign({}, state, { step: state.step - 1 });
+    case 'PREVIOUS_STEP': {
+      // Special condition for skipping step 2 and going straight to step 1
+      const decrement = state.step === 3 && state.steps[1].fields.type.value === 'individual' ? 2 : 1;
+
+      return Object.assign({}, state, {
+        step: state.step - decrement,
+        shouldRender: true,
+      });
+    }
 
     case 'VALIDATE_FORM': {
       const newState = Object.assign({}, state);
-      const formStep = state.steps[`step${state.step}`];
+      const stepFields = state.steps[state.step].fields;
       let stepScore = 0;
+      let neededScore = 0;
 
       // assign the value to its model
-      newState.steps[`step${state.step}`][action.payload.field].value = action.payload.value;
-      newState.steps[`step${state.step}`][action.payload.field].isValid = action.payload.isValid;
+      newState.steps[state.step].fields[action.payload.field].value = action.payload.value;
+      newState.steps[state.step].fields[action.payload.field].isValid = action.payload.isValid;
 
-      stepScore = Object.keys(formStep)
-        .map(field => formStep[field].isValid)
+      stepScore = Object.keys(stepFields)
+        .map(field => stepFields[field].isValid || false)
         .reduce((score = 0, value) => score + value);
-      newState.isValid = stepScore === Object.keys(formStep);
+
+      neededScore = Object.keys(stepFields)
+        .filter(field => stepFields[field].required).length;
+
+      newState.steps[state.step].isValid = stepScore >= neededScore;
 
       newState.shouldRender = false;
 
@@ -81,8 +129,7 @@ class Survey {
             </div>
           </form>
         </div>
-      </div>
-    `;
+      </div>`;
     this.$el = this.build(this.template);
 
     store.subscribe(this.update.bind(this));
@@ -90,16 +137,16 @@ class Survey {
 
   build(content) {
     const _f = document.createElement('div');
-
     _f.innerHTML = content;
-
     return _f;
   }
 
   render() {
+    // sets the events up
     this.$el.addEventListener('click', this.handleClick.bind(this));
     this.$el.addEventListener('change', this.handleChange.bind(this));
     this.$el.addEventListener('keyup', this.handleChange.bind(this));
+    this.$el.addEventListener('submit', this.handleSubmit.bind(this));
 
     this.update();
 
@@ -110,60 +157,138 @@ class Survey {
 
   update() {
     const state = store.getState();
+    const $title = this.$el.querySelector('#survey-title');
     const $step = this.$el.querySelector('#survey-body');
     const $prevButton = this.$el.querySelector('[data-action="prev-step"]');
     const $nextButton = this.$el.querySelector('[data-action="next-step"]');
     const stepTemplates = {
       1: `
-        <input name="name" type="text" placeholder="Name" required
-          value="${state.steps.step1.name.value}">
-        <input name="email" type="email" placeholder="Email" required
-          value="${state.steps.step1.email.value}">
-        <select name="type" required>
-          <option value="" selected disabled>Select an option</option>
-          <option value="business"
-            ${state.steps.step1.type.value === 'business' && 'selected'}>Business</option>
-          <option value="individual"
-            ${state.steps.step1.type.value === 'individual' && 'selected'}>Individual</option>
-        </select>
-      `,
-      2: 'step 2',
-      3: 'step 3',
-      4: 'step 4',
-      5: 'step 5',
+        <div class="input-container">
+          <label>Name:</label>
+          <input name="name" type="text" placeholder="Name" required
+            value="${state.steps[1].fields.name.value}">
+        </div>
+        <div class="input-container">
+          <label>Email:</label>
+          <input name="email" type="email" placeholder="Email" required
+            value="${state.steps[1].fields.email.value}">
+        </div>
+        <div class="input-container">
+          <label>Type:</label>
+          <select name="type" required>
+            <option value="" selected disabled>Select an option</option>
+            <option value="business"
+              ${state.steps[1].fields.type.value === 'business' && 'selected'}>Business</option>
+            <option value="individual"
+              ${state.steps[1].fields.type.value === 'individual' && 'selected'}>Individual</option>
+          </select>
+        </div>`,
+      2: `
+        <div class="input-container">
+          <label>Business name:</label>
+          <input name="business.name" type="text" placeholder="Business name" required
+            value="${state.steps[2].fields['business.name'].value}">
+        </div>
+        <div class="input-container">
+          <label>Number of employees:</label>
+          <select name="business.size" required>
+            <option value="" selected disabled>Select a business size</option>
+            <option value="1-10"
+              ${state.steps[2].fields['business.size'].value === '1-10' && 'selected'}>1-10</option>
+            <option value="11-25"
+              ${state.steps[2].fields['business.size'].value === '11-25' && 'selected'}>11-25</option>
+            <option value="26-50"
+              ${state.steps[2].fields['business.size'].value === '26-50' && 'selected'}>26-50</option>
+            <option value="51-100"
+              ${state.steps[2].fields['business.size'].value === '51-100' && 'selected'}>51-100</option>
+            <option value="+100"
+              ${state.steps[2].fields['business.size'].value === '+100' && 'selected'}>+100</option>
+          </select>
+        </div>`,
+      3: `
+        <div class="input-container">
+          <label>Address:</label>
+          <input name="address" type="text" placeholder="Address" required
+            value="${state.steps[3].fields.address.value}">
+        </div>
+        <div class="input-container">
+          <span>Residing in the EU?</span>
+          <label>
+            <input name="euresident" type="radio" value="1" required
+              ${state.steps[3].fields.euresident.value === '1' && 'checked'}>
+            <span>Yes</span>
+          </label>
+          <label>
+            <input name="euresident" type="radio" value="0" required
+              ${state.steps[3].fields.euresident.value === '0' && 'checked'}>
+            <span>No</span>
+          </label>
+        </div>`,
+      4: `
+        <div class="input-container">
+          <label>Favourite book:</label>
+          <input name="book" type="text" placeholder="Type the name of the book"
+            value="${state.steps[4].fields.book.value}">
+        </div>
+        <div class="input-container">
+          <span>Favourite colors:</span>
+          <label>
+            <input type="checkbox" value="red" name="colors.red"
+              ${state.steps[4].fields['colors.red'].value === 'red' ? 'checked' : ''}>
+            <span>Red</span>
+          </label>
+          <label>
+            <input type="checkbox" value="green" name="colors.green"
+              ${state.steps[4].fields['colors.green'].value === 'green' ? 'checked' : ''}>
+            <span>Green</span>
+          </label>
+          <label>
+            <input type="checkbox" value="blue" name="colors.blue"
+              ${state.steps[4].fields['colors.blue'].value === 'blue' ? 'checked' : ''}>
+            <span>Blue</span>
+          </label>
+        </div>
+        `,
+      5: `
+        <div id="survey-result">
+          <h2>Thanks for your help!</h2>
+        </div>
+        `,
     };
+
+    // update title
+    $title.innerHTML = state.steps[state.step].title;
 
     // update step
     if (state.shouldRender) $step.innerHTML = stepTemplates[state.step];
 
     // update footer
     $prevButton.style.display = state.step === 1 ? 'none' : '';
-    $nextButton.style.display = state.step === state.totalSteps ? 'none' : '';
+    $nextButton.style.display = state.step === Object.keys(stepTemplates).length ? 'none' : '';
   }
 
   handleClick(e) {
-    e.preventDefault();
-
     switch (e.target.getAttribute('data-action')) {
-      case 'prev-step': {
-        store.dispatch(setPrevStep());
-        break;
-      }
+      case 'prev-step': store.dispatch(setPrevStep()); break;
       case 'next-step': store.dispatch(setNextStep()); break;
       default: return true;
     }
 
-    return this;
+    return true;
   }
 
   handleChange(e) {
-    e.preventDefault();
+    // save the form in LS
 
     store.dispatch(validateForm({
       field: e.target.name,
       value: e.target.value,
       isValid: e.target.validity.valid,
     }));
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
   }
 }
 
