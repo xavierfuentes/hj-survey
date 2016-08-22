72beb874be4b9dd837a61401df6b875c6a6e34ec
Xavi <xavifuefer@gmail.com>
2016-08-22 10:32:18 +0100

    create demo environment

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,20 @@
 const path = require('path');
 const webpack = require('webpack');
 
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
 module.exports = {
   devtool: 'inline-source-map',
-  entry: [
-    'webpack-dev-server/client?http://localhost:8080',
-    'webpack/hot/only-dev-server',
-    path.resolve(__dirname, 'app/index.js'),
-  ],
+  entry: {
+    app: [
+      'webpack-dev-server/client?http://localhost:8080',
+      'webpack/hot/only-dev-server',
+      path.resolve(__dirname, 'app/index.js'),
+    ],
+    widget: [
+      'webpack-dev-server/client?http://localhost:8080',
+      'webpack/hot/only-dev-server',
+      path.resolve(__dirname, 'widget/index.js'),
+    ],
+  },
   module: {
     loaders: [
       { test: /\.js?$/, exclude: /node_modules/, loader: 'babel' },
@@ -22,7 +27,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'dist'),
     publicPath: '/',
-    filename: 'bundle.js',
+    filename: '[name].js',
   },
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
@@ -30,10 +35,5 @@ module.exports = {
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
-    new HtmlWebpackPlugin({
-      template: 'app/index.html',
-      inject: 'body',
-      hash: true,
-    }),
   ],
 };
